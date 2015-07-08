8ea18e995827db45db778b589c4d1783c0520efb
mkaczma <m.kaczmarek1993@interia.pl>
2015-07-08 10:52:08 +0200

    fix bug

--- src/main/java/com/acme/order/notification/SimpleMessageTemplateService.java
+++ src/main/java/com/acme/order/notification/SimpleMessageTemplateService.java
@@ -1,9 +1,14 @@
 package com.acme.order.notification;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class SimpleMessageTemplateService implements MessageTemplateService {
 
-	private final DeliveryTemplate deliveryTemplate;
-	private final OrderCancelledTemplate cancelDeliveryTemplate;
+	private DeliveryTemplate deliveryTemplate;
+	private OrderCancelledTemplate cancelDeliveryTemplate;
 
 	public SimpleMessageTemplateService() {
 		this.deliveryTemplate = new DeliveryTemplate();
